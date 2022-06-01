c257bdd777b90178e0ec1b8f2cd6ca9797676b33
Laptop Asus <stipro150197@gmail.com>
2022-06-01 16:41:29 -0500

    Cambio de direccion inicial de sistema

--- app/config/bee_config.php
+++ app/config/bee_config.php
@@ -8,7 +8,7 @@
  * configuración especial en producción
  */
 define('IS_LOCAL'     , in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']));
-define('BASEPATH'     , IS_LOCAL ? '/medifer/' : 'yasur.soy.pe/medifer/');
+define('BASEPATH'     , IS_LOCAL ? '/medifer/' : '/medifer/');
 define('IS_DEMO'      , false);
 
 // Set para conexión en producción o servidor real
